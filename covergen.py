1781673773ade548c7456ccdc5c38da741117122
Akim Kingori <akim.king12@gmail.com>
2021-05-29 10:06:25 +0300

    refactor

--- covergen.py
+++ covergen.py
@@ -27,22 +27,9 @@ count = 0
 
 result = Image.new("RGB", (1000, 1000))
 
-for i in range(3):
-    if i != 2:
-        for index in range(len(images) - 1):
-            path = os.path.expanduser(images[index])
-            new_image = Image.open(path)
-            new_size = 2 ** -(i + 1) * 1000
-            new_image.thumbnail((new_size, new_size), Image.ANTIALIAS)
-            x = positions[count][0]
-            y = positions[count][1]
-            width, height = new_image.size
-            print(index)
-            print('pos {0},{1} size {2},{3}'.format(x, y, width, height))
-            result.paste(new_image, (x, y, x + width, y + height))
-            count += 1
-    elif i == 2:
-        for index2 in range(len(images)):
+def paste_images(length):
+    global count
+    for index2 in range(length):
             path = os.path.expanduser(images[index2])
             new_image = Image.open(path)
             new_size = 2 ** -(i + 1) * 1000
@@ -54,5 +41,13 @@ for i in range(3):
             print('pos {0},{1} size {2},{3}'.format(x, y, width, height))
             result.paste(new_image, (x, y, x + width, y + height))
             count += 1
+
+for i in range(3):
+    if i != 2:
+        length = len(images) - 1
+        paste_images(length)
+    elif i == 2:
+        length = len(images)
+        paste_images(length)
 
 result.save(os.path.expanduser('./output/cover.jpg'))
